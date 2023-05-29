7b05223405168556cac049a0f430254175a3856c
Mounir El-Bakkali <mounir.elbakkali1@etu.uae.ac.ma>
2023-05-29 12:27:39 +0100

    init setup for client config

---
+++ src/main/java/ma/youcode/demoservice/DemoServiceApplication.java
@@ -0,0 +1,13 @@
+package ma.youcode.demoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/youcode/demoservice/DemoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.youcode.demoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
