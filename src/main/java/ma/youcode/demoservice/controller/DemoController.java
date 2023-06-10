a37c3aff0426aaa1d9b6957407b3e106f59e336f
Mounir El-Bakkali <mounir.elbakkali1@etu.uae.ac.ma>
2023-06-10 19:30:32 +0100

    demo config endpoint

--- src/main/java/ma/youcode/demoservice/controller/DemoController.java
+++ src/main/java/ma/youcode/demoservice/controller/DemoController.java
@@ -1,2 +1,28 @@
-package ma.youcode.demoservice.controller;public class DemoController {
+package ma.youcode.demoservice.controller;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+@RequestMapping("/demo/config")
+public class DemoController {
+
+
+    @Value("${server.port}")
+    String port ;
+
+    @Value("${spring.datasource.url}")
+    String dataSourceURL ;
+
+
+    @GetMapping
+    public String getApplicationProperties(){
+        return String.format("server port is %s and data source URL : %s",port , dataSourceURL);
+    }
 }
